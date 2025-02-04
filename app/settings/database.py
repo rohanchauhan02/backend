d6a2fab84c0dddff957383eee912d4b52c161bde
rohanchauhan02 <rohan021195@gmail.com>
2025-02-04 15:40:25 +0530

    fix changes

--- app/settings/database.py
+++ app/settings/database.py
@@ -1,16 +1,14 @@
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-from os import environ
+import os
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": environ.get("DB_NAME"),
-        "USER": environ.get("DB_USER"),
-        "PASSWORD": environ.get("DB_PASSWORD"),
-        "HOST": environ.get("DB_HOST"),
-        "PORT": environ.get("DB_PORT"),
-       'OPTIONS': {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE', 'backend'),
+        'USER': os.getenv('MYSQL_USER', 'root'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'root'),
+        'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
+        'PORT': os.getenv('MYSQL_PORT', '3306'),
+        'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
